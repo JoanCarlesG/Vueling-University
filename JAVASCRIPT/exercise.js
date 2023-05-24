0327fbe7148b280c00a81bb0a1b539ffe69bd8d3
Joan Carles <joan.godinez@col.vueling.com>
2023-05-24 11:16:31 +0200

    first js exercise

---
+++ JAVASCRIPT/exercise.js
@@ -0,0 +1,12 @@
+
+let age = 20;
+
+function askAge(age){
+    if (age < 18){
+        console.log("No eres mayor de edad");
+    } else{
+        console.log("Eres mayor de edad")
+    }
+}
+
+askAge(age);
